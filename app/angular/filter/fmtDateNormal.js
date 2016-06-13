d3f976ce332d9c2ddff11c93c120f11d767e0ccb
Manjiz <25199230@qq.com>
2016-06-13 10:47:33 +0800

    优化格式化时间fmtDateNormal的代码

--- app/angular/filter/fmtDateNormal.js
+++ app/angular/filter/fmtDateNormal.js
@@ -2,40 +2,28 @@
 
 angular.module('mine.fmtDateNormal', []).filter('fmtDateNormal', [function() {
     return function(text) {
-        var date = new Date(text);
-        if(date=='Invalid Date') {
-            return '';
-        }
+        var past = new Date(text);
+
+        if(past=='Invalid Date') { return ''; }
+
         var now = new Date(),
-            sepdays = Math.floor(now.getTime()/86400000) - Math.floor(date.getTime()/86400000);
-        if(sepdays>7) {
-            if(now.getFullYear()-date.getFullYear()>0) {
-                return date.getFullYear()+'年'+(date.getMonth()+1)+'月'+date.getDate()+'日';
+            septime = now.getTime() - past.getTime();
+
+        // 大于7天
+        if(septime>604800000) {
+            if(now.getFullYear()-past.getFullYear()>0) {
+                return past.getFullYear()+'年'+(past.getMonth()+1)+'月'+past.getDate()+'日';
             } else {
-                return (date.getMonth()+1)+'月'+date.getDate()+'日';
+                return (past.getMonth()+1)+'月'+past.getDate()+'日';
             }
-        } else if(sepdays>0) {
-            return sepdays + '天前';
+        } else if(septime>86400000) {
+            return Math.floor(septime/86400000)+'天前';
+        } else if(septime>3600000) {
+            return Math.floor(septime/3600000)+'小时前';
+        } else if(septime>60000) {
+            return Math.floor(septime/60000)+'分钟前';
         } else {
-            var seph = now.getHours()-date.getHours();
-            var sepm = now.getMinutes()-date.getMinutes();
-            var seps = now.getSeconds()-date.getSeconds();
-            if(seph>0) {
-                // 60-(now-past) = 60-past+now，(sepm+60)<60 等同 sepm<0
-                if(seph===1 && sepm<0) {
-                    return (sepm+60)+'分钟前';
-                } else {
-                    return seph+'小时前';
-                }
-            } else if(sepm>0) {
-                if(sepm===1 && seps<0) {
-                    return (seps+60)+'秒前';
-                } else {
-                    return sepm+'分钟前';
-                }
-            } else {
-                return seps+'秒前';
-            }
+            return Math.floor(septime/1000)+'秒前';
         }
     }
 }])
